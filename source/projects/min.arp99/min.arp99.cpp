cf1ed02eb15f68afcab75b18ab60ef84c96e01ed
Brad Osgood <bosgood@gmail.com>
2020-08-12 23:29:11 -0400

    arp99 checkin

--- source/projects/min.arp99/min.arp99.cpp
+++ source/projects/min.arp99/min.arp99.cpp
@@ -10,9 +10,9 @@ using namespace c74::min;
 class arp99 : public object<arp99> {
 public:
   MIN_DESCRIPTION { "Pattern generator based on the Mutable Instruments Yarns module." };
-  MIN_TAGS { };
+  MIN_TAGS { "time" };
   MIN_AUTHOR { "bosgood" };
-  MIN_RELATED {};
+  MIN_RELATED		{ "min.beat.random, link.beat, metro, tempo, drunk" };
 
   inlet<> clock { this, "(bang) clock signal input" };
   inlet<> pitch1_in { this, "(float) note 1 pitch" };
@@ -22,17 +22,30 @@ public:
   outlet<> trig_out { this, "(bang) note trigger signal" };
   outlet<> pitch_out { this, "(float) note pitch" };
 
-  timer<> metro { this,
-    MIN_FUNCTION {
+//   timer<> metro { this,
+//     MIN_FUNCTION {
+//       cout << "metro" << endl;
+//       metro.delay(1000);
+//       pitch_out.send(0.0);
+//       trig_out.send("bang");
+//       return {};
+//     }
+//   };
 
-      return {};
-    }
-  };
+//   message<> bang { this, "bang",
+//     MIN_FUNCTION {
+//       cout << "bang received" << endl;
+//       return {};
+//     }
+//   };
 
-private:
-  // The timestamps of the few clocks signals received, used for
-  // tempo calculation
-  int clock_signals { 0, 0, 0 };
+// private:
+//   // The beats per minute value as currently calculated from the
+//   // incoming clock signals
+//   int bpm = 120;
+//   // The timestamps of the few clocks signals received, used for
+//   // tempo calculation
+//   int clock_signals[3] { 0, 0, 0 };
 };
 
-MAX_EXTERNAL(arp99);
+MIN_EXTERNAL(arp99);
