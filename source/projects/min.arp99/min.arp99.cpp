2f84ce970a3281ae5f514b04eda502cb901eed9d
Brad Osgood <bosgood@gmail.com>
2020-08-16 22:58:36 -0400

    Implementing BPM counting via clock signals

--- source/projects/min.arp99/min.arp99.cpp
+++ source/projects/min.arp99/min.arp99.cpp
@@ -10,29 +10,38 @@ using namespace c74::min;
 
 class arp99 : public object<arp99> {
 public:
-    MIN_DESCRIPTION	{ "Bang at intervals in a repeating pattern." };
-    MIN_TAGS		{ "time" };
-    MIN_AUTHOR		{ "Cycling '74" };
-    MIN_RELATED		{ "min.beat.random, link.beat, metro, tempo, drunk" };
+    MIN_DESCRIPTION	{ "Arpeggiator deluxe" };
+    MIN_TAGS	    	{ "time" };
+    MIN_AUTHOR	  	{ "bosgood" };
+    MIN_RELATED	    { "min.beat.random, link.beat, metro, tempo, drunk" };
 
-    inlet<>  input			{ this, "(toggle) on/off brad asdfas" };
-    inlet<>  input2			{ this, "(toggle) on/off 2" };
+    inlet<>  input			{ this, "(toggle) on/off" };
+    inlet<>  clock_in		{ this, "(bang) clock signal input" };
     outlet<> bang_out		{ this, "(bang) triggers at according to specified pattern" };
-    outlet<> interval_out	{ this, "(float) the interval for the current bang" };
+    outlet<> pitch_out	{ this, "(float) the pitch value for the current bang" };
+
+    // inlet<> clock       { this, "(bang) clock signal input" };
+    // inlet<> pitch1_in   { this, "(float) note 1 pitch" };
+    // inlet<> velocity1_in { this, "(float) note 1 velocity" };
+    // inlet<> pitch2_in { this, "(float) note 2 pitch" };
 
     timer<> metro { this,
         MIN_FUNCTION {
-            double interval = m_sequence[m_index];
+            cout << "arp tick" << endl;
 
-            interval_out.send(interval);
+            // double pitch = m_sequence[m_index];
+
+            pitch_out.send(0);
             bang_out.send("bang");
 
-            metro.delay(interval);
+            // Calculate millisecond delay interval from current beats per minute value
+            double ms_delay = m_tempo / 60 / 1000;
+            metro.delay(ms_delay);
 
-            m_index += 1;
+            // m_index += 1;
 
-            if (m_index == m_sequence.size())
-              m_index = 0;
+            // if (m_index == m_sequence.size())
+            //   m_index = 0;
             return {};
         }
     };
@@ -51,22 +60,23 @@ public:
     message<> toggle { this, "int", "Turn on/off the internal timer.",
         MIN_FUNCTION {
             on = args[0];
+            cout << "Toggled arp: " << on << endl;
             return {};
         }
     };
 
-
-    message<> dictionary { this, "dictionary", "Use a dictionary to define the pattern of bangs produced.",
+    message<> clock { this, "bang", "Receive an incoming clock signal.",
         MIN_FUNCTION {
-            dict d {args[0]};
-
-            m_sequence = d["pattern"];
-            return {};
+          cout << "Clock tick received" << endl;
+          return {};
         }
     };
 
 private:
-    int   m_index		{ 0 };
+    // Current tempo as measured in beats per minute
+    int   m_tempo     { 120 };
+    int   m_index	    { 0 };
+    std::vector<int> m_clock_signals { 0, 0, 0 };
     atoms m_sequence	{ 250.0, 250.0, 250.0, 250.0, 500.0, 500.0, 500.0, 500.0 };
 };
 
