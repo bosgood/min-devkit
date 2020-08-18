46be4ac5ae3fb0f73122798885cf26bffe53d915
Brad Osgood <bosgood@gmail.com>
2020-08-18 00:14:05 -0400

    Implement note handling, begin tempo sync

--- source/projects/min.arp99/min.arp99.cpp
+++ source/projects/min.arp99/min.arp99.cpp
@@ -18,47 +18,44 @@ public:
     MIN_AUTHOR	  	{ "bosgood" };
     MIN_RELATED	    { "min.beat.random, link.beat, metro, tempo, drunk" };
 
-    inlet<>  input			{ this, "(toggle) on/off" };
-    inlet<>  clock_in		{ this, "(bang) clock signal input" };
+    inlet<>  tempo_in   { this, "(int) tempo input" };
+    inlet<>  clock_in		{ this, "(float) clock signal input" };
+    inlet<>  pitch_in   { this, "(list) pitch in" };
+    inlet<>  cc_in      { this, "(list) cc in" };
     outlet<> bang_out		{ this, "(bang) triggers at according to specified pattern" };
     outlet<> pitch_out	{ this, "(float) the pitch value for the current bang" };
     outlet<> tempo_out  { this, "(float) the current tempo (BPM)"};
 
-    // inlet<> clock       { this, "(bang) clock signal input" };
-    // inlet<> pitch1_in   { this, "(float) note 1 pitch" };
-    // inlet<> velocity1_in { this, "(float) note 1 velocity" };
-    // inlet<> pitch2_in { this, "(float) note 2 pitch" };
+    // timer<> metro { this,
+    //     MIN_FUNCTION {
+    //         // double pitch = m_sequence[m_index];
+    //         pitch_out.send(0);
+    //         bang_out.send("bang");
+    //         tempo_out.send(m_tempo);
 
-    timer<> metro { this,
-        MIN_FUNCTION {
-            // double pitch = m_sequence[m_index];
-            pitch_out.send(0);
-            bang_out.send("bang");
-            tempo_out.send(m_tempo);
+    //         // Calculate millisecond delay interval from current beats per minute value
+    //         double ms_delay = m_tempo / m_clock_div / 60 * 1000;
+    //         cout << "arp tick (delay: " << ms_delay << "ms) " << endl;
+    //         metro.delay(ms_delay);
 
-            // Calculate millisecond delay interval from current beats per minute value
-            double ms_delay = m_tempo / m_clock_div / 60 * 1000;
-            cout << "arp tick (delay: " << ms_delay << "ms) " << endl;
-            metro.delay(ms_delay);
+    //         // m_index += 1;
 
-            // m_index += 1;
+    //         // if (m_index == m_sequence.size())
+    //         //   m_index = 0;
+    //         return {};
+    //     }
+    // };
 
-            // if (m_index == m_sequence.size())
-            //   m_index = 0;
-            return {};
-        }
-    };
-
-    attribute<bool> on {this, "on", false,
-        description {"Turn on/off the internal timer."},
-        setter { MIN_FUNCTION {
-            if (args[0] == true)
-                metro.delay(0.0);    // fire the first one straight-away
-            else
-                metro.stop();
-            return args;
-        }}
-    };
+    // attribute<bool> on {this, "on", false,
+    //     description {"Turn on/off the internal timer."},
+    //     setter { MIN_FUNCTION {
+    //         if (args[0] == true)
+    //             metro.delay(0.0);    // fire the first one straight-away
+    //         else
+    //             metro.stop();
+    //         return args;
+    //     }}
+    // };
 
     attribute<int> clock_div {this, "clock_div", 16,
         description {"Clock division"},
@@ -69,30 +66,76 @@ public:
         }}
     };
 
-    message<> toggle { this, "int", "Turn on/off the internal timer.",
+    // message<> toggle { this, "int", "Turn on/off the internal timer.",
+    //     MIN_FUNCTION {
+    //         on = args[0];
+    //         cout << "Toggled arp: " << on << endl;
+    //         return {};
+    //     }
+    // };
+
+    // message<> clock { this, "bang", "Receive an incoming clock signal.",
+    //     MIN_FUNCTION {
+    //       auto now = std::chrono::system_clock::now();
+    //       auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    //       auto epoch = now_ms.time_since_epoch();
+    //       m_clock_signals[m_tick_index] = epoch;
+
+    //       auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
+    //       cout << "Clock tick received at " << value.count() << endl;
+    //       m_tick_index = (m_tick_index + 1) % 3;
+
+    //       // Use the last few clock signals received to calculate
+    //       // the current tempo
+
+    //       // TODO [bosgood] Implement tempo calc
+
+    //       return {};
+    //     }
+    // };
+
+    message<> clock { this, "int", "Receive an incoming clock signal.",
         MIN_FUNCTION {
-            on = args[0];
-            cout << "Toggled arp: " << on << endl;
-            return {};
+          cout << "Clock signal received" << endl;
+          return {};
         }
     };
 
-    message<> clock { this, "bang", "Receive an incoming clock signal.",
-        MIN_FUNCTION {
-          cout << "Clock tick received" << endl;
-          auto now = std::chrono::system_clock::now();
-          auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-          auto epoch = now_ms.time_since_epoch();
-          m_clock_signals[m_tick_index] = epoch;
-          m_tick_index = (m_tick_index + 1) % 3;
-
-          // Use the last few clock signals received to calculate
-          // the current tempo
-
-          // TODO [bosgood] Implement tempo calc
-
-          return {};
+    message<> tempo { this, "float", "Tempo input",
+      MIN_FUNCTION {
+        if (inlet == 0) {
+          m_tempo = args[0];
+          cout << "Received tempo: " << m_tempo << endl;
         }
+        return {};
+      }
+    };
+
+    message<> list { this, "list", "Pitch input",
+      MIN_FUNCTION {
+        if (inlet == 2) {
+          // Args are pairs of (Pitch, Velocity)
+          for (auto i = 0; i < args.size(); i += 2) {
+            auto pitch = args[i];
+            auto velocity = (int)args[i+1];
+            if (velocity > 0) {
+              cout << "NOTEON " << pitch << endl;
+              m_notes[pitch] = 1;
+            } else {
+              cout << "NOTEOFF " << pitch << endl;
+              m_notes[pitch] = 0;
+            }
+          }
+        } else if (inlet == 3) {
+          // Args are pairs of (CC, Value)
+          for (auto i = 0; i < args.size(); i += 2) {
+            auto cc = args[i];
+            auto val = args[i+1];
+            cout << "CC " << cc << " val " << val << endl;
+          }
+        }
+        return {};
+      }
     };
 
 private:
@@ -101,6 +144,9 @@ private:
     int m_index	{ 0 };
     int m_clock_div { 16 };
     int m_tick_index { 0 };
+    std::vector<int> m_notes = {
+      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    };
     std::vector<std::chrono::milliseconds> m_clock_signals {
       std::chrono::milliseconds { 0 },
       std::chrono::milliseconds { 0 },
